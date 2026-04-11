fb1fe7964dbba452674b8257a0e629a6bd5467a9
Huy Phuoc <156315187+zrtnhp0406@users.noreply.github.com>
2026-04-12 01:00:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import torch
 from torch.utils.data import DataLoader
 from transformers import BertTokenizerFast
 from dataset import create_aspect_dataset, prepare_training_data
-from arch import model_Bert
+from arch import ESIM_BERT
 from train import train_model
 from transformers import get_linear_schedule_with_warmup
 import torch.optim as optim
@@ -17,14 +17,13 @@ val_reviews = pd.read_csv(r"C:\work\Research_Same_User_Diff_Opinion\Data\reviews
 
 
 train_aspect_reviews = create_aspect_dataset(train_sentences, contain_aspect_sentences=True)
-test_aspect_reviews = create_aspect_dataset(test_sentences, contain_aspect_sentences=True)
+test_aspect_reviews= create_aspect_dataset(test_sentences, contain_aspect_sentences=True)
 val_aspect_reviews = create_aspect_dataset(val_sentences, contain_aspect_sentences=True)
 
-train_df = prepare_training_data(train_reviews, train_aspect_reviews)
-val_df = prepare_training_data(val_reviews, val_aspect_reviews)
-
-model = model_Bert(freeze_layers=8)
-optimizer = torch.optim.AdamW(filter(lambda p: p.requires_grad, model.parameters()), lr=2e-4, weight_decay=0.01)
+train_df,fp_train,fn_train = prepare_training_data(train_reviews, train_aspect_reviews)
+val_df,fp_val,fn_val = prepare_training_data(val_reviews, val_aspect_reviews)
+test_df,fp_test,fn_test = prepare_training_data(test_reviews, test_aspect_reviews)
+model = ESIM_BERT(freeze_layers=9)
 tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
 
-best_model = train_model(model, train_df, val_df, tokenizer, optimizer, num_epochs=10, batch_size=16)
+best_model = train_model(model, train_df, val_df, test_df, fp_train, fn_train, fp_test, fn_test, fp_val, fn_val,tokenizer, num_epochs=10, batch_size=16)
